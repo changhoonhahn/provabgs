510c6a1a5a1eb557728fa834535e2247622463bc
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2021-02-11 17:45:44 -0500

    6 wavelength bin PCA has low reconstruction error

--- bin/pca.py
+++ bin/pca.py
@@ -10,7 +10,8 @@ import numpy as np
 from speculator import SpectrumPCA
 
 
-dat_dir='/global/cscratch1/sd/chahah/provabgs/' # hardcoded to NERSC directory 
+#dat_dir='/global/cscratch1/sd/chahah/provabgs/' # hardcoded to NERSC directory 
+dat_dir='/tigress/chhahn/provabgs/'
 
 
 def divide_trainingset_3wavebins(name, batch0, batch1): 
@@ -37,6 +38,43 @@ def divide_trainingset_3wavebins(name, batch0, batch1):
         np.save(fspec.replace('.npy', '.w0.npy'), spec[:,wave_bin0])
         np.save(fspec.replace('.npy', '.w1.npy'), spec[:,wave_bin1])
         np.save(fspec.replace('.npy', '.w2.npy'), spec[:,wave_bin2])
+
+    return None 
+
+
+def divide_trainingset_6wavebins(name, batch0, batch1): 
+    ''' divide DESI training set into 6 wavelength intervals
+    1. (wave < 3000) 
+    2. (wave >= 3000) & (wave < 4000) 
+    3. (wave >= 4000) & (wave < 5000) 
+    4. (wave >= 5000) & (wave < 6000) 
+    5. (wave >= 6000) & (wave < 7000) 
+    6. (wave >= 7000)
+    '''
+    # fsps wavelength 
+    fwave   = os.path.join(dat_dir, 'wave_fsps.npy') 
+    wave    = np.load(fwave)
+
+    wave_bin0 = (wave < 3000) 
+    wave_bin1 = (wave >= 3000) & (wave < 4000) 
+    wave_bin2 = (wave >= 4000) & (wave < 5000) 
+    wave_bin3 = (wave >= 5000) & (wave < 6000) 
+    wave_bin4 = (wave >= 6000) & (wave < 7000) 
+    wave_bin5 = (wave >= 7000)
+
+    # batches of fsps spectra
+    batches = range(batch0, batch1+1)
+    fspecs  = [os.path.join(dat_dir, 
+        'fsps.%s.lnspectrum.seed%i.npy' % (name, ibatch)) for ibatch in batches]
+
+    for fspec in fspecs: 
+        spec = np.load(fspec)
+        np.save(fspec.replace('.npy', '.6w0.npy'), spec[:,wave_bin0])
+        np.save(fspec.replace('.npy', '.6w1.npy'), spec[:,wave_bin1])
+        np.save(fspec.replace('.npy', '.6w2.npy'), spec[:,wave_bin2])
+        np.save(fspec.replace('.npy', '.6w3.npy'), spec[:,wave_bin3])
+        np.save(fspec.replace('.npy', '.6w4.npy'), spec[:,wave_bin4])
+        np.save(fspec.replace('.npy', '.6w5.npy'), spec[:,wave_bin5])
 
     return None 
 
@@ -100,6 +138,64 @@ def train_pca_3wavebins(name, batch0, batch1, n_pca, i_bin):
     return None 
 
 
+def train_pca_6wavebins(name, batch0, batch1, n_pca, i_bin): 
+    ''' train PCA for DESI simpledust or complexdust training sets for 3 wavelength
+    intervals: 
+    '''
+    # fsps wavelength 
+    fwave   = os.path.join(dat_dir, 'wave_fsps.npy') 
+    wave    = np.load(fwave)
+
+    wave_bin0 = (wave < 3000) 
+    wave_bin1 = (wave >= 3000) & (wave < 4000) 
+    wave_bin2 = (wave >= 4000) & (wave < 5000) 
+    wave_bin3 = (wave >= 5000) & (wave < 6000) 
+    wave_bin4 = (wave >= 6000) & (wave < 7000) 
+    wave_bin5 = (wave >= 7000)
+
+    # batches of fsps spectra
+    batches = range(batch0, batch1+1)
+    
+    # parameters 
+    fthetas = [os.path.join(dat_dir, 'fsps.%s.theta.seed%i.npy' % (name,
+        ibatch)) for ibatch in batches]
+    
+    wave_bin = [wave_bin0, wave_bin1, wave_bin2, wave_bin3, wave_bin4, wave_bin5][i_bin]
+
+    # log(spectra) over wavelength bin 
+    fspecs  = [os.path.join(dat_dir, 
+        'fsps.%s.lnspectrum.seed%i.6w%i.npy' % (name, ibatch, i_bin)) for
+        ibatch in batches]
+
+    if name == 'nmf_bases': 
+        # theta = [b1, b2, b3, b4, g1, g2, dust1, dust2, dust_index, zred]
+        n_param = 10 
+    elif name == 'nmfburst': 
+        # theta = [b1, b2, b3, b4, fburst, tburst, g1, g2, dust1, dust2, dust_index, zred]
+        n_param = 12 
+    n_wave = np.sum(wave_bin) 
+    
+    # train PCA basis 
+    PCABasis = SpectrumPCA(
+            n_parameters=n_param,       # number of parameters
+            n_wavelengths=n_wave,       # number of wavelength values
+            n_pcas=n_pca,               # number of pca coefficients to include in the basis 
+            spectrum_filenames=fspecs,  # list of filenames containing the (un-normalized) log spectra for training the PCA
+            parameter_filenames=fthetas, # list of filenames containing the corresponding parameter values
+            parameter_selection=None) 
+
+    PCABasis.compute_spectrum_parameters_shift_and_scale() # computes shifts and scales for (log) spectra and parameters
+    PCABasis.train_pca()
+    PCABasis.transform_and_stack_training_data(
+            os.path.join(dat_dir, 'fsps.%s.seed%i_%i.6w%i.pca%i' % (name, batch0, batch1, i_bin, n_pca)), 
+            retain=True) 
+    # save to file 
+    PCABasis._save_to_file(
+            os.path.join(dat_dir, 'fsps.%s.seed%i_%i.6w%i.pca%i.hdf5' % (name, batch0, batch1, i_bin, n_pca))
+            )
+    return None 
+
+
 if __name__=="__main__": 
     job     = sys.argv[1]
     name    = sys.argv[2]
@@ -107,8 +203,18 @@ if __name__=="__main__":
     ibatch1 = int(sys.argv[4])
     
     if job == 'train': 
-        n_pca = int(sys.argv[5]) 
-        i_bin = int(sys.argv[6])
-        train_pca_3wavebins(name, ibatch0, ibatch1, n_pca, i_bin)
+        n_wavebins = int(sys.argv[5])
+        n_pca = int(sys.argv[6]) 
+        i_bin = int(sys.argv[7])
+
+        if n_wavebins == 3: 
+            train_pca_3wavebins(name, ibatch0, ibatch1, n_pca, i_bin)
+        elif n_wavebins == 6: 
+            train_pca_6wavebins(name, ibatch0, ibatch1, n_pca, i_bin)
+
     elif job == 'divide': 
-        divide_trainingset_3wavebins(name, ibatch0, ibatch1) 
+        n_wavebins = int(sys.argv[5])
+        if n_wavebins == 3: 
+            divide_trainingset_3wavebins(name, ibatch0, ibatch1) 
+        elif n_wavebins == 6: 
+            divide_trainingset_6wavebins(name, ibatch0, ibatch1) 
--- bin/validate_pca.py
+++ bin/validate_pca.py
@@ -4,6 +4,8 @@ import numpy as np
 from speculator import SpectrumPCA
 from speculator import Speculator
 
+import matplotlib as mpl 
+mpl.use('Agg') 
 import matplotlib.pyplot as plt
 
 
@@ -11,7 +13,7 @@ import matplotlib.pyplot as plt
 model = 'nmfburst'
 
 # number of PCA components 
-n_pcas  = [70, 30, 30]
+n_pcas  = [30, 60, 30, 30, 30, 30]
 
 # number of parameters
 if model == 'nmf_bases':
@@ -19,29 +21,38 @@ if model == 'nmf_bases':
 elif model == 'nmfburst': 
     n_param = 12 
 
-dat_dir = '/Users/chahah/data/provabgs/'
+#dat_dir = '/Users/chahah/data/provabgs/'
+dat_dir='/tigress/chhahn/provabgs/'
 if 'NERSC_HOST' in os.environ: 
     dat_dir = '/global/cscratch1/sd/chahah/provabgs/'
 
 # read in wavelenght values 
 wave = np.load(os.path.join(dat_dir, 'wave_fsps.npy'))
 
-wave_bins = [(wave < 4500), (wave >= 4500) & (wave < 6500), (wave >= 6500)]
+wave_bins = [
+        (wave < 3000),
+        (wave >= 3000) & (wave < 4000),
+        (wave >= 4000) & (wave < 5000),
+        (wave >= 5000) & (wave < 6000),
+        (wave >= 6000) & (wave < 7000),
+        (wave >= 7000)]
+
 
 # read in SpectrumPCA objects
 PCABases = []
 for i in range(len(n_pcas)):
+    n_wave = np.sum(wave_bins[i]) 
     # load trained PCA basis object
     PCABasis = SpectrumPCA( 
             n_parameters=n_param,       # number of parameters
-            n_wavelengths=np.sum(wave_bins[i]),       # number of wavelength values
+            n_wavelengths=n_wave,       # number of wavelength values
             n_pcas=n_pcas[i],              # number of pca coefficients to include in the basis
             spectrum_filenames=None,  # list of filenames containing the (un-normalized) log spectra for training the PCA
             parameter_filenames=[], # list of filenames containing the corresponding parameter values
             parameter_selection=None) # pass an optional function that takes in parameter vector(s) and returns True/False for any extra parameter cuts we want to impose on the training sample (eg we may want to restrict the parameter ranges)
 
     fpca = os.path.join(dat_dir, 
-            'fsps.%s.seed0_999.w%i.pca%i.hdf5' % (model, i, n_pcas[i]))
+            'fsps.%s.seed0_499.6w%i.pca%i.hdf5' % (model, i, n_pcas[i]))
     PCABasis._load_from_file(fpca) 
     PCABases.append(PCABasis)
 
@@ -49,18 +60,13 @@ for i in range(len(n_pcas)):
 # read in test parameters and data
 theta_test      = np.load(os.path.join(dat_dir,
     'fsps.%s.theta.test.npy' % model))
-_lnspec_test    = np.load(os.path.join(dat_dir, 
+lnspec_test    = np.load(os.path.join(dat_dir, 
     'fsps.%s.lnspectrum.test.npy' % model))
-
-lnspec_test = []
-for wave_bin in wave_bins: 
-    lnspec_test.append(_lnspec_test[:,wave_bin])
-
 
 # calculate PCA reconstructed spectra for test set 
 lnspec_recon = []
 for i in range(len(wave_bins)):
-    _spec = lnspec_test[i]
+    _spec = lnspec_test[:,wave_bins[i]]
     normalized_spec = (_spec - PCABases[i].spectrum_shift) / PCABases[i].spectrum_scale 
 
     # transform to PCA basis and back
@@ -69,38 +75,50 @@ for i in range(len(wave_bins)):
         PCABases[i].pca_transform_matrix)*PCABases[i].spectrum_scale +
         PCABases[i].spectrum_shift)
 
+lnspec_recon = np.concatenate(lnspec_recon, axis=1)
+
+
+# plot the fraction error 
+frac_dspectrum = 1. - np.exp(lnspec_recon - lnspec_test) 
+frac_dspectrum_quantiles = np.nanquantile(frac_dspectrum, 
+        [0.0005, 0.005, 0.025, 0.5, 0.975, 0.995, 0.9995], axis=0)
 
 fig = plt.figure(figsize=(15,5))
-for iwave in range(len(wave_bins)): 
-    # calculate fraction error 
-    frac_dspectrum = 1. - np.exp(lnspec_recon[iwave] - lnspec_test[iwave]) 
-    frac_dspectrum_quantiles = np.nanquantile(frac_dspectrum, 
-            [0.0005, 0.005, 0.025, 0.5, 0.975, 0.995, 0.9995], axis=0)
+sub = fig.add_subplot(111)
+sub.fill_between(wave, frac_dspectrum_quantiles[0],
+        frac_dspectrum_quantiles[6], fc='C0', ec='none', alpha=0.1, label='99.9%')
+sub.fill_between(wave, frac_dspectrum_quantiles[1],
+        frac_dspectrum_quantiles[5], fc='C0', ec='none', alpha=0.2, label='99%')
+sub.fill_between(wave, frac_dspectrum_quantiles[2],
+        frac_dspectrum_quantiles[4], fc='C0', ec='none', alpha=0.3, label='95%')
+sub.plot(wave, frac_dspectrum_quantiles[3], c='C0', ls='-') 
+sub.plot(wave, np.zeros(len(wave)), c='k', ls=':') 
+for wave_bin in wave_bins: 
+    sub.vlines(wave[wave_bin][-1], -0.1, 0.1, color='k', linestyle=':')
 
-    sub = fig.add_subplot(1,3,iwave+1)
-    sub.fill_between(wave[wave_bins[iwave]], frac_dspectrum_quantiles[0],
-            frac_dspectrum_quantiles[6], fc='C0', ec='none', alpha=0.1, label='99.9%')
-    sub.fill_between(wave[wave_bins[iwave]], frac_dspectrum_quantiles[1],
-            frac_dspectrum_quantiles[5], fc='C0', ec='none', alpha=0.2, label='99%')
-    sub.fill_between(wave[wave_bins[iwave]], frac_dspectrum_quantiles[2],
-            frac_dspectrum_quantiles[4], fc='C0', ec='none', alpha=0.3, label='95%')
-    sub.plot(wave[wave_bins[iwave]], frac_dspectrum_quantiles[3], c='C0', ls='-') 
-    sub.plot(wave, np.zeros(len(wave)), c='k', ls=':') 
+# mark +/- 1%
+sub.plot(wave, 0.01 * np.ones(len(wave)), c='k', ls='--', lw=0.5)
+sub.plot(wave, -0.01 * np.ones(len(wave)), c='k', ls='--', lw=0.5)
 
-    # mark +/- 1%
-    sub.plot(wave, 0.01 * np.ones(len(wave)), c='k', ls='--', lw=0.5)
-    sub.plot(wave, -0.01 * np.ones(len(wave)), c='k', ls='--', lw=0.5)
+sub.legend(loc='upper right', fontsize=20)
+sub.set_xlabel('wavelength', fontsize=25) 
+sub.set_xlim(2.3e3, 1e4)
+sub.set_ylabel(r'$(f_{\rm pca} - f_{\rm fsps})/f_{\rm fsps}$', fontsize=25) 
+sub.set_ylim(-0.03, 0.03) 
 
-    if iwave == len(wave_bins) - 1: sub.legend(loc='upper right', fontsize=20)
-    if iwave == 0: 
-        sub.set_xlim(2.3e3, 4.5e3)
-    elif iwave == 1: 
-        sub.set_xlabel('wavelength ($A$)', fontsize=25) 
-        sub.set_xlim(4.5e3, 6.5e3)
-    elif iwave == 2: 
-        sub.set_xlim(6.5e3, 1e4)
-    if iwave == 0: sub.set_ylabel(r'$(f_{\rm pca} - f_{\rm fsps})/f_{\rm fsps}$', fontsize=25) 
-    sub.set_ylim(-0.03, 0.03) 
-    if iwave != 0 : sub.set_yticklabels([])
+fig.savefig('fsps.%s.valid_pca.%s.png' % (model, '_'.join([str(np) for np in n_pcas])), bbox_inches='tight') 
 
-fig.savefig('fsps.%s.valid_pca.%i_%i_%i.png' % (model, n_pcas[0], n_pcas[1], n_pcas[2]), bbox_inches='tight') 
+
+# plot CDF of fractional reconstruction error
+mean_frac_dspectrum = np.mean(np.abs(1. - np.exp(lnspec_recon - lnspec_test)), axis=1)
+quant = np.quantile(mean_frac_dspectrum, [0.68, 0.95, 0.99, 0.999])
+fig = plt.figure(figsize=(8,6))
+sub = fig.add_subplot(111)
+for q, a in zip(quant[::-1], [0.1, 0.2, 0.3, 0.5]): 
+    sub.fill_between([0., q], [0., 0.], [1., 1.], alpha=a, color='C0')
+_ = sub.hist(mean_frac_dspectrum, 40, density=True, histtype='step', cumulative=True, color='k')
+sub.set_xlabel(r'${\rm mean}_\lambda \langle (f_{\rm speculator}  - f_{\rm fsps}) / f_{\rm fsps} \rangle$', fontsize=20)
+sub.set_xlim(0., 0.03)
+sub.set_ylabel('cumulative distribution', fontsize=20)
+sub.set_ylim(0., 1.)
+fig.savefig('fsps.%s.valid_pca.%s.cdf.png' % (model, '_'.join([str(np) for np in n_pcas])), bbox_inches='tight') 
