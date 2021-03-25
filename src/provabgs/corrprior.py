0726c0b6d10e7e035d2df526e00b5c89085f73ed
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2021-03-25 18:36:59 -0400

    corrprior now supports ranges on derived parameters

--- src/provabgs/corrprior.py
+++ src/provabgs/corrprior.py
@@ -4,6 +4,7 @@ module for imposing prior correction
 
 '''
 import numpy as np 
+from scipy.stats import uniform
 
 
 class CorrectPrior(object): 
@@ -49,7 +50,7 @@ class CorrectPrior(object):
     *
     '''
     def __init__(self, model, prior, tage=None, zred=None, props=['logmstar',
-        'avgsfr_1gyr', 'z_mw'], Nprior=100000, method='kde', debug=False,
+        'avgsfr_1gyr', 'z_mw'], Nprior=100000, method='kde', range=None, debug=False,
         **method_kwargs):
 
         self.model = model
@@ -60,6 +61,13 @@ class CorrectPrior(object):
         self.props = props 
         self.Nprior = Nprior  
         self.method = method
+        self.range = range 
+        if self.range is not None: 
+            self.range = np.atleast_2d(self.range)
+            self._shift = np.array(self.range)[:,0]
+            self._uniform = uniform(
+                    np.array(self.range)[:,0] - self._shift, 
+                    np.array(self.range)[:,1] - self._shift) 
 
         # check that properties are named 
         self.supported_props = ['logmstar', 'avgsfr_1gyr', 'logavgsfr_1gyr',
@@ -70,7 +78,7 @@ class CorrectPrior(object):
                     (prop, ', '.join(self.supported_props)))
 
         # fit the prior on specified properties
-        self._fit_prior(debug=debug, **method_kwargs)
+        self._fit_prior(range=self.range, debug=debug, **method_kwargs)
 
     def get_importance_weights(self, theta, outlier=0.01, debug=False): 
         ''' calculate importance weights for given parameter values. The input
@@ -88,23 +96,28 @@ class CorrectPrior(object):
         ftheta = self._get_properties(theta) 
     
         p_ftheta = self.p_ftheta.pdf(ftheta)
+        
+        if outlier is not None:
+            # clip probabilities that are on the edges 
+            low_lim, high_lim = np.percentile(p_ftheta[p_ftheta > 0.], 
+                    [0.5*outlier, 100.-0.5*outlier]) 
+            if debug: print('... clipping values outside %.5e, %.5e' % (low_lim, high_lim))
 
-        # clip probabilities that are on the edges 
-        low_lim, high_lim = np.percentile(p_ftheta, 
-                [0.5*outlier, 100.-0.5*outlier]) 
-        if debug: print('... clipping values outside %.5e, %.5e' % (low_lim, high_lim))
+            ws = 1./np.clip(p_ftheta, low_lim, high_lim)
+        else: 
+            ws = 1./p_ftheta
 
-        return 1./np.clip(p_ftheta, low_lim, high_lim) 
+        if self.range is not None: 
+            # if X is out of range return 0
+            inrange = np.prod(self._uniform.pdf(np.atleast_2d(ftheta) - self._shift), axis=1).astype(bool)
+            ws[~inrange] = 0.
+        return ws
 
-    def _clip_prob(self, probs): 
-        '''
-        '''
-
-    def _fit_prior(self, debug=False, **method_kwargs): 
+    def _fit_prior(self, range=None, debug=False, **method_kwargs): 
         ''' fit the prior distribution of derived properties.
         '''
         # sample Nprior thetas from prior 
-        _theta_prior = np.array([self.prior.sample() for i in range(self.Nprior)])
+        _theta_prior = np.array([self.prior.sample() for i in np.arange(self.Nprior)])
         theta_prior = self.prior.transform(_theta_prior)
         
         # get specified properties from theta and model
@@ -112,7 +125,7 @@ class CorrectPrior(object):
         
         # fit PDF of derived properties
         if debug: print('... fitting prior(derived prop)') 
-        self.p_ftheta = _fit_pdf(ftheta, method=self.method, debug=debug, **method_kwargs)
+        self.p_ftheta = _fit_pdf(ftheta, method=self.method, range=range, debug=debug, **method_kwargs)
         return None 
 
     def _get_properties(self, theta, debug=False): 
@@ -159,7 +172,7 @@ class CorrectPrior(object):
         return np.array(derived_props).T 
 
 
-def _fit_pdf(samples, method='kde', debug=False, **method_kwargs): 
+def _fit_pdf(samples, method='kde', range=None, debug=False, **method_kwargs): 
     ''' fit a probability distribution sampled by given samples using method
     specified by `method`. This function is designed to fit p(F(theta)), the
     probability distribution of *derived* properties; however, it works for any
@@ -176,11 +189,19 @@ def _fit_pdf(samples, method='kde', debug=False, **method_kwargs):
         'gmm' (default: 'kde') 
     '''
     if debug: print('... fitting pdf using %s' % method) 
+
+    # whiten samples
+    avg_samples = np.mean(samples, axis=0) 
+    std_samples = np.std(samples, axis=0) 
+    samples_w = (samples - avg_samples) / std_samples
+
     if method == 'kde': 
         # fit PDF using Kernel Density Estimation 
-        from scipy.stats import gaussian_kde as gkde
+        #from scipy.stats import gaussian_kde as gkde
+        from sklearn.neighbors import KernelDensity
 
-        pdf_fit = gkde(samples.T, **method_kwargs)
+        pdf_fit = KernelDensity(kernel='gaussian',
+                **method_kwargs).fit(samples_w)
     else: 
         from sklearn.mixture import GaussianMixture as GMix
 
@@ -188,28 +209,68 @@ def _fit_pdf(samples, method='kde', debug=False, **method_kwargs):
             raise ValueError("specify number of Gaussians `n_comp` in kwargs") 
 
         gmm = GMix(n_components=method_kwargs['n_comp'])
-        gmm.fit(samples)
+        gmm.fit(samples_w)
         pdf_fit = gmm
         
-    return _PDF(pdf_fit, method=method) 
+    return _PDF(pdf_fit, method=method, range=range, avg=avg_samples,
+            std=std_samples) 
 
 
 class _PDF(object): 
     ''' wrapper for different PDF fit objects in order to evaluate the pdf at
     and sample it conveniently.
     '''
-    def __init__(self, pdf_fit, method='kde'): 
+    def __init__(self, pdf_fit, method='kde', range=None, avg=None, std=None): 
         self.pdf_fit = pdf_fit 
         self.method = method 
+        self.range = range 
+        if self.range is not None: 
+            lim0 = np.array(self.range)[:,0]
+            lim1 = np.array(self.range)[:,1]
+            self._shift = lim0 
+            self._uniform = uniform(lim0 - self._shift, lim1 - self._shift) 
 
-    def pdf(self, X): 
-        if self.method == 'kde': 
-            return self.pdf_fit.pdf(X.T)
-        elif self.method == 'gmm': 
-            return np.exp(self.pdf_fit.score_samples(X))
+        self.avg = avg
+        self.std = std 
+
+    def pdf(self, _X): 
+        X = (_X - self.avg)/self.std
+        _pdf = np.exp(self.pdf_fit.score_samples(X))
+
+        if self.range is not None: 
+            # if X is out of range return 0
+            inrange = self._inrange(_X)
+            _pdf[~inrange] = 0.
+        return _pdf
+
+    def log_pdf(self, _X): 
+        X = (_X - self.avg)/self.std
+        _lnpdf = self.pdf_fit.score_samples(X)
+        if self.range is not None: 
+            # if X is out of range return -np.inf
+            inrange = self._inrange(_X)
+            _lnpdf[~inrange] = -np.inf
+        return _lnpdf 
 
     def sample(self, Nsample): 
+        rvs = self._sample(Nsample)
+
+        if self.range is not None: 
+            inrange = self._inrange(rvs)
+            # impose range here
+            while np.sum(~inrange) > 0: 
+                rvs[~inrange] = self._sample(np.sum(~inrange))
+                inrange = self._inrange(rvs)
+
+            inrange = self._inrange(rvs)
+        return rvs
+
+    def _sample(self, Nsample): 
+        rvs = self.pdf_fit.sample(Nsample)
         if self.method == 'kde': 
-            return self.pdf_fit.resample(Nsample)
+            return rvs * self.std + self.avg
         elif self.method == 'gmm': 
-            return np.exp(self.pdf_fit.sample(Nsample))
+            return rvs[0] * self.std + self.avg
+
+    def _inrange(self, X):
+        return np.prod(self._uniform.pdf(np.atleast_2d(X) - self._shift[None,:]), axis=1).astype(bool)
