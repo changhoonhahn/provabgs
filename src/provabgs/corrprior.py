e42e2d83bee5abfeb795d260661f2c6dbdaf1d65
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2021-03-26 16:39:37 -0400

    validation plot implemented for corrprior

--- src/provabgs/corrprior.py
+++ src/provabgs/corrprior.py
@@ -113,6 +113,53 @@ class CorrectPrior(object):
             ws[~inrange] = 0.
         return ws
 
+    def validate(self, Nvalidate=10000, outlier=0.1):
+        ''' generate validation plot for corrprior object. The figure will
+        plot the original f(theta) prior distribution to the corrprior fit to
+        p(f(theta)) and the corrected distribution.
+
+        Parameters
+        ----------
+        Nvalidate : int
+            number of sample to draw from the prior and the fit for validation.
+            (default : 10000) 
+        '''
+        # sample Nprior thetas from prior 
+        _theta_prior = np.array([self.prior.sample() for i in np.arange(self.Nvalidate)])
+        theta_prior = self.prior.transform(_theta_prior)
+
+        # get specified properties from theta and model
+        ftheta_prior = self._get_properties(theta_prior) 
+    
+        # draw samples from the p(f(theta)) fit 
+        ftheta_valid = self.p_ftheta.sample(Nvalidate) 
+    
+        # get importance weights
+        w_imp = self.get_importance_weights(theta_prior, outlier=outlier)
+    
+        if len(self.props) == 1: 
+            # plot 1d histogram 
+            fig = plt.figure(figsize=(6,6))
+            sub = fig.add_subplot(111)
+            _ = sub.hist(ftheta_prior, density=True, color='C0')
+            _ = sub.hist(ftheta_valid, density=True, color='C1', alpha=0.9)
+            _ = sub.hist(ftheta_prior, weights=w_imp, density=True, color='C2',
+                    alpha=0.8)
+            sub.set_xlim(np.min(np.concatenate([ftheta_prior, ftheta_valid])),
+                    np.max(np.concatenate([ftheta_prior, ftheta_valid]))) 
+        else:
+            import corner as DFM 
+            # plot comparison corner plot 
+            fig = DFM.corner(ftheta_prior, hist_kwargs={'density': True},
+                    plot_datapoints=False, plot_density=False, color='C0')
+            _ = DFM.corner(ftheta_valid, hist_kwargs={'density': True},
+                    plot_datapoints=False, plot_density=False, color='C1',
+                    fig=fig)
+            _ = DFM.corner(ftheta_prior, weights=w_imp, hist_kwargs={'density':
+                True}, plot_datapoints=False, plot_density=False, color='C2', 
+                fig=fig)
+        return fig 
+
     def _fit_prior(self, range=None, debug=False, **method_kwargs): 
         ''' fit the prior distribution of derived properties.
         '''
