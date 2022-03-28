37e1e3c7b26299fd7662d3aadd2aa3511dd7e0b2
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2022-03-28 17:39:22 -0400

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+        'logo': 'logo.png'
+        }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
